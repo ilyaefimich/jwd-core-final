20061ba4f1be2421e4fb5ded9f39992b9d143f8d
ilyaefimich <ilyaefimich@gmail.com>
2020-11-11 23:53:29 +0300

    Add Property Reader implementation.

--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -1,10 +1,28 @@
 package com.epam.jwd.core_final;
 
 import com.epam.jwd.core_final.context.Application;
+import com.epam.jwd.core_final.context.ApplicationMenu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    static final Logger logger = LoggerFactory.getLogger("JWDCourseLogger");
 
     public static void main(String[] args) {
-        Application.start();
+        try {
+            logger.trace("Main: The application has been started.");
+
+            ApplicationMenu mainMenu = Application.start();
+            logger.trace("Main: ApplicationMenu has been initialised.");
+
+            while (true) {
+                mainMenu.printAvailableOptions();
+                if ((Integer)mainMenu.handleUserInput(null) == 1) return;
+            }
+
+        } catch (Exception e) {
+            logger.error("Main: Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/epam/jwd/core_final/util/PropertyReaderUtil.java
+++ src/main/java/com/epam/jwd/core_final/util/PropertyReaderUtil.java
@@ -1,7 +1,11 @@
 package com.epam.jwd.core_final.util;
 
 import com.epam.jwd.core_final.domain.ApplicationProperties;
+import com.epam.jwd.core_final.exception.FileParseException;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public final class PropertyReaderUtil {
@@ -14,13 +18,28 @@ public final class PropertyReaderUtil {
     /**
      * try-with-resource using FileInputStream
      *
-     * @see {https://www.netjstech.com/2017/09/how-to-read-properties-file-in-java.html for an example}
      * <p>
      * as a result - you should populate {@link ApplicationProperties} with corresponding
      * values from property file
      */
     public static void loadProperties() {
-        final String propertiesFileName = "resource/application.properties";
+        final String propertiesFileName = "src/main/resources/application.properties";
+        InputStream iStream = null;
+        try {
+            iStream = new FileInputStream(propertiesFileName);
+            properties.load(iStream);
+            ApplicationProperties.populate(properties);
 
+        } catch (IOException e) {
+            throw new FileParseException(propertiesFileName);
+        } finally {
+            try {
+                if(iStream != null){
+                    iStream.close();
+                }
+            } catch (IOException e) {
+                throw new FileParseException(propertiesFileName);
+            }
+        }
     }
 }
